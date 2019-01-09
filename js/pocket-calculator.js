3f78fa1cd3e70b598479bd4d14e90baa03b2b5cf
aarjoon <43757252+aarjoon@users.noreply.github.com>
2019-01-09 12:50:12 -0500

    Update pocket-calculator.js

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,39 +1,50 @@
-function numOne() {
+var evaluateStr = '';
+var lastNum = true; var lastSym = false; var lastEql = false;
 
+function insert(num){
+  if (lastEql){
+    clean(); lastEql = false;
+  }
+  if (lastNum){
+    document.calculator.display.value = (document.calculator.display.value + num);
+    lastSym = true;
+  } else {
+    document.calculator.display.value = (num);
+    lastSym = true; lastNum = true;
+  }
+    evaluateStr = evaluateStr + num;
+
+    if(Number(document.calculator.display.value.length) > 11){
+    document.calculator.display.value = "Too many digits";
+    }
+}
+function parseSym(sym){
+  if (lastSym){
+    document.calculator.display.value = (sym);
+    lastSym = false; lastNum = false; lastEql = false; evaluateStr = evaluateStr + sym;
+  }
+}
+function equal(){
+  document.calculator.display.value = eval(evaluateStr).toLocaleString("en");
+  evaluateStr = eval(evaluateStr); lastNum = false; lastEql = true;
+  if(Number(evaluateStr) > 1000000000) {
+         let coefficient = 0;
+         let degree;
+         for(let n = 9; coefficient < 1 || coefficient >= 10; n++) {
+           coefficient = evaluateStr / 10 ** n;
+           degree = n;
+         }
+         document.calculator.display.value = String(coefficient) + "e" + String(degree);
+  }
+}
+function clean(){
+  document.calculator.display.value = ' ';
+  evaluateStr = ''; lastNum = true; lastSym = false;
+}
+function percentage(){
+  document.calculator.display.value = document.calculator.display.value/100;
 }
 
-function numTwo() {
-
-}
-
-function numThree() {
-
-}
-
-function numFour() {
-
-}
-
-function numFive() {
-
-}
-
-function numSix() {
-
-}
-
-function numSeven() {
-
-}
-
-function numEight() {
-
-}
-
-function numNine() {
-
-}
-
-function numZero() {
-
+function negation(){
+document.calculator.display.value = document.calculator.display.value * -1;
 }
